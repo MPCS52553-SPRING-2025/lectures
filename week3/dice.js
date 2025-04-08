3dc0edbd07094653314142a4afd5486648cd78a8
Jeff Cohen <cohen.jeff@gmail.com>
2025-04-07 19:35:39 -0500

    remove mistaken files

--- week3/dice.js
+++
@@ -1,6 +0,0 @@
-// This is a helpful function to generate a random number from 1 to 6
-const pickRandomNumber = function() {
-  return Math.floor(Math.random() * 6) + 1;
-}
-
-// Your code goes here...
