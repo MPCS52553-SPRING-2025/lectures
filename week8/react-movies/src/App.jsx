dda33157dd84b70d1d862b1afe2eaa679349ccdc
Jeff Cohen <cohen.jeff@gmail.com>
2025-05-12 11:33:42 -0500

    Ready for Week 8

---
+++ week8/react-movies/src/App.jsx
@@ -0,0 +1,129 @@
+import { React, useState } from 'react';
+
+function Movie(props) {
+  return (
+    <div className="col-sm-3 text-center mb-2 poster">
+      <img src={'http://image.tmdb.org/t/p/w185' + props.poster_path} className="img-fluid" alt={props.title} />
+
+      <p className="mt-2">
+        {props.release_date ? props.release_date.substr(0, 4) : null}
+        <span className="badge bg-primary mx-3">
+          {props.vote_average.toFixed(1)}
+        </span>
+        <LikeButton title={props.title} onButtonClick={props.onLikeClicked} likeCount={props.likeCount}/>
+      </p>
+    </div>
+  )
+}
+
+function LikeButton(props) {
+
+  function handleLikeClicked(e) {
+    e.preventDefault()
+    props.onButtonClick(props.title)
+  }
+  return (
+    <button onClick={handleLikeClicked}  className="text-decoration-none btn text-danger ">&hearts; 
+      <span>{props.likeCount}</span>
+    </button>
+  )
+}
+
+function App() {
+
+  let initialData = [
+    { title: "The Princess Bride", poster_path: '/dvjqlp2sAhUeFjUOfQDgqwpphHj.jpg', release_date: '1999', vote_average: 10 },
+    { title: "Spider - Man", poster_path: '/gh4cZbhZxyTbgxQPxD0dOudNPTn.jpg', release_date: '1999', vote_average: 10 },
+    { title: "Star Wars", poster_path: '/6FfCtAuVAW8XJjZ7eWeLibRLWTw.jpg', release_date: '1999', vote_average: 10 },
+    { title: "Jurassic Park", poster_path: 'https://image.tmdb.org/t/p/w185//b1xCNnyrPebIc7EWNZIa6jhb1Ww.jpg', release_date: '1993', vote_average: 10 },
+    { title: "Doctor Strange", poster_path: '/7WfK17BXE6szXlm4WOxfswgbdsL.jpg', release_date: '1999', vote_average: 10 },
+    { title: "Apollo 13", poster_path: '/kzj95o9FlVxKziQq36mjES3wxel.jpg', release_date: '1999', vote_average: 10 },
+    { title: "The Matrix", poster_path: '//f89U3ADr1oiB1s9GkdPOEpXUk5H.jpg', release_date: '1999', vote_average: 10 },
+    { title: "Toy Story", poster_path: '/uXDfjJbdP4ijW5hWSBrPrlKpxab.jpg', release_date: '1999', vote_average: 10 },
+  ]
+  
+  const [data, setData] = useState(initialData)
+  const [likes, setLikes] = useState({}) // { "Apollo 13": 100, "Princess Bride": 5, ...}
+
+  function incrementLikes(title) {
+    // { "Apollo 13": 8, "Princess Bride": 5, ...}
+    const currentLikeCount = likes[title] || 0
+    let obj = { }
+    obj[title] = currentLikeCount + 1   // obj: { "Princess Bride": 6 }
+    const newLikes = { ...likes, ...obj }    
+    setLikes(newLikes)
+  }
+
+
+  const movies = data.map(movie_data => <Movie onLikeClicked={incrementLikes}  likeCount={likes[movie_data.title] || 0} key={movie_data.title} title={movie_data.title} release_date={movie_data.release_date} poster_path={movie_data.poster_path} vote_average={movie_data.vote_average} />)
+
+  function handleNowPlayingButton(event) {
+    console.log(event)
+    event.preventDefault();
+    const url = urlForMovies("now_playing")
+    fetch(url).then(response => response.json()).then(api_data => setData(api_data.results))
+  }
+
+  function handleTopRatedButton(event) {
+    console.log(event)
+    event.preventDefault();
+    const url = urlForMovies("top_rated")
+    fetch(url).then(response => response.json()).then(api_data => setData(api_data.results))
+  }
+  function handleOurMoviesButton(event) {
+    console.log(event)
+    event.preventDefault();
+    setData(initialData)
+  }
+
+  return (
+    <div>
+      <header className="row mb-5 justify-content-between">
+        <form className="col-sm-4">
+          <input className="form-control" autoFocus name="search" type="text" placeholder="Search by title...">
+          </input>
+        </form>
+
+        <p className="mt-2">
+          <button onClick={handleTopRatedButton} className="btn btn-primary" >Top-Rated Movies</button>
+          <button onClick={handleNowPlayingButton} className="btn btn-primary ms-3">Now Playing</button>
+          <button onClick={handleOurMoviesButton} className="btn btn-secondary ms-3">Our Movies</button>
+        </p>
+      </header>
+
+      <div className="row justify-content-between flex-wrap gy-5" id="movies">
+        {movies}
+      </div>
+    </div>
+  )
+}
+
+function getApiKey() { return "bde024f3eb43f597aafe01ed9c9098c6" }
+
+// Pass in the "resource" you want to retrieve: 'top_movies' or 'now_playing'
+function urlForMovies(resource) {
+  const apiKey = `api_key=${getApiKey()}`
+  const language = "language=en-US"
+  const region = "region=US"
+  const filter = "include_adult=false"
+  const base_url = `https://api.themoviedb.org/3/movie/${resource}`
+  return `${base_url}?${apiKey}&${language}&${region}&${filter}`
+}
+
+// Pass in the movite title or keyword to search for
+function urlForSearch(keyword) {
+  const apiKey = `api_key=${getApiKey()}`
+  const language = "language=en-US"
+  const search = `query=${keyword}`
+  const region = "region=US"
+  const filter = "include_adult=false"
+  const base_url = `https://api.themoviedb.org/3/search/movie`
+  return `${base_url}?${apiKey}&${search}&${language}&${region}&${filter}`
+}
+
+export default App;
+
+// To make development easier, we can make certain functions available to the browser
+// by adding a reference into the built-in window object
+window.urlForMovies = urlForMovies
+window.urlForSearch = urlForSearch
---
+++ week8/react-shopping/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import './styles/App.css';
+import Cart from './Cart';
+import Totals from './Totals';
+import AddItemForm from './AddItemForm';
+import Header from './Header'
+
+function App() {
+  const [items, setItems] = useState([])
+
+  function calculateSubtotal() {
+    let subtotal = 0
+    for (const item of items) {
+      subtotal += item.price * item.qty
+    }
+    return subtotal
+  }
+  
+  function handleItemChanged(item) {
+    const newItems = items.map(existing_item => {
+      if (existing_item.name === item.name) {
+        // return a new object that has the updated new qty
+        return { name: item.name, qty: item.qty, price: item.price }
+      } else {
+        // leave item unchanged
+        return existing_item
+      }
+    })
+    setItems(newItems)
+  }
+
+  function handleAddItem(itemName) {
+    const qty = 1
+    const price = items.length + 1
+    const newItems = [...items, { name: itemName, qty: qty, price: price }]
+    setItems(newItems)
+  }
+
+  const subtotal = calculateSubtotal()
+
+  return (
+    <>
+      <Header/>
+      <AddItemForm onAddItem={handleAddItem} />
+      
+      <div className="flex space-between" id="main">
+        <Cart items={items} onItemChanged={handleItemChanged}/>
+        <Totals amount={subtotal}/>
+      </div>
+    </>
+  )
+}
+
+export default App;
